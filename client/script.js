4b0ec945a9b36367acf18377f95a82f9b50c61e3
RykiRytkonen <rytkonen.santeri123@gmail.com>
2023-10-11 14:54:49 +0300

    Node modules go away

---
+++ client/script.js
@@ -0,0 +1,94 @@
+/*
+
+function fetchDataAndUpdate() {
+    fetch('/data')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            document.getElementById('modeP').textContent = `Current mode: ${data.mode}`;
+            document.getElementById('pressureP').textContent = `Current pressure: ${data.pressure}`;
+            document.getElementById('fanSpeedP').textContent = `Current fan speed: ${data.fanSpeed}`;
+            document.getElementById('co2P').textContent = `Current co2: ${data.co2}`;
+            document.getElementById('tempP').textContent = `Current temperature: ${data.temp}`;
+            document.getElementById('humidityP').textContent = `Current humidity: ${data.humidity}`;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+setInterval(fetchDataAndUpdate, 1000);
+
+*/
+
+document.getElementById('switch').addEventListener('click', function(){
+    if(document.getElementById('switch').checked) {
+        document.getElementById('slider').max = '100';
+        document.getElementById('slider').value = '50'
+        sliderValue.textContent = '50'
+        document.getElementById('sliderText').textContent = 'Fan Speed';
+    }
+    else {
+        document.getElementById('slider').max = '120';
+        document.getElementById('slider').value = '60'
+        sliderValue.textContent = '60'
+        document.getElementById('sliderText').textContent = 'Pressure';
+    }
+});
+
+const url = "ws://localhost:3001"
+const mywsServer = new WebSocket(url)
+
+document.getElementById('manualButton').addEventListener('click', function() {
+    let url = "";
+    let value = document.getElementById('slider').value;
+    if(document.getElementById('switch').checked) {
+        url = `/data?mode=manual&speed=${value}`
+    }
+    else {
+
+        url = `/data?mode=automatic&pressure=${value}`
+    }
+    /*
+    fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });*/
+
+    fetch(url);
+});
+
+mywsServer.onmessage = function(event) {
+    const { data } = event
+    let d = JSON.parse(data)
+    console.log(data)
+    if(d.auto){
+        document.getElementById('modeP').textContent = `Current mode: Automatic`
+        document.getElementById('pressureP').textContent = `Current pressure: ${d.pressure}`;
+    }
+    else {
+        document.getElementById('modeP').textContent = `Current mode: Manual`;
+        document.getElementById('fanSpeedP').textContent = `Current fan speed: ${d.speed}`;
+    }
+    if(d.pressure != undefined){
+        document.getElementById('pressureP').textContent = `Current pressure: ${d.pressure} Pa`;
+        document.getElementById('fanSpeedP').textContent = `Current fan speed: ${d.speed}`;
+        document.getElementById('co2P').textContent = `Current co2: ${d.co2}`;
+        document.getElementById('tempP').textContent = `Current temperature: ${d.temp} C`;
+        document.getElementById('humidityP').textContent = `Current humidity: ${d.rh} %`;
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var slider = document.getElementById('slider')
+    var sliderValue = document.getElementById('sliderValue')
+    document.getElementById('slider').value = '60'
+    sliderValue.textContent = slider.value
+    document.getElementById('switch').checked = false
+
+    slider.addEventListener('input', function() {
+        sliderValue.textContent = slider.value
+    });
+});
---
+++ server/server.js
@@ -0,0 +1,284 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const WebSocket = require("ws");
+const mqtt = require("mqtt");
+const mqtt_client = mqtt.connect("mqtt://192.168.1.106:1883");
+
+
+app.use(express.static(path.join(__dirname, '../client')));
+app.set('views', path.join(__dirname, '../client/views'));
+app.use(express.json());
+
+let db = new sqlite3.Database('../data/myDatabase.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the SQLite database.');
+});
+
+mqtt_client.on("connect", () => {
+    mqtt_client.subscribe("controller/status", (err) => {
+      if (!err) {
+        console.log("Subscribed to controller/status")
+      }
+  
+    });
+  });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve("../client/index.html"))
+});
+
+app.get('/data', (req, res) => {
+
+    console.log(req.query.mode)
+    if(req.query.mode == "automatic"){
+      console.log(req.query.pressure)
+      mqtt_client.publish("controller/settings", JSON.stringify({"auto": true, "pressure": +req.query.pressure}), {}, (err) => {
+        if(err){
+          console.error("MQTT Publssh Error", err)
+          res.status(500).send("Failed to publis MQTT Message")
+        } else {
+          console.log("Message published")
+          res.send("Message published")
+        }
+      })
+    }
+    else {
+      console.log(req.query.speed)
+      mqtt_client.publish("controller/settings", JSON.stringify({"auto": false, "speed": +req.query.speed}), {}, (err) => {
+        if(err){
+          console.error("MQTT Publssh Error", err)
+          res.status(500).send("Failed to publis MQTT Message")
+        } else {
+          console.log("Message published")
+          res.send("Message published")
+        }
+      })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+        process.exit(0);
+    });
+});
+
+function execute(query, parameters = []){
+    return new Promise((resolve, reject) => {
+      db.run(query, parameters, (error) => {
+        if(error){
+          reject(error)
+        }
+        resolve()
+      });
+    })
+  }
+  
+  function find_all(query, parameters){
+    console.log(query)
+    console.log(parameters)
+    return new Promise((resolve, reject) => {
+      db.all(query, parameters, (error, rows) => {
+        if(error){
+          reject(error)
+        }
+        resolve(rows)
+      });
+    })
+  }
+
+const myServer = app.listen(3001)
+
+const wsServer = new WebSocket.Server({
+  noServer: true
+})
+
+wsServer.on("connection", function(ws) {    // what should a websocket do on connection
+console.log("New connection")
+ws.on("message", function(msg) {        // what to do on message event
+    wsServer.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {     // check if client is ready
+        client.send(msg.toString());
+    }
+    })
+})
+})
+  
+function init_db(){
+db.run(`CREATE TABLE IF NOT EXISTS measurement(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nr INTEGER,
+            time_stamp INTEGER,
+            speed INTEGER,
+            setpoint INTEGER,
+            pressure INTEGER,
+            auto INTEGER,
+            error INTEGER,
+            co2 INTEGER,
+            rh INTEGER,
+            temperature INTEGER
+            );
+`, (error) => {
+    if(error){
+    console.log(error)
+    process.exit()
+    }
+    else {
+    console.log("Initialized DB")
+    }
+})
+}
+
+myServer.on('upgrade', async function upgrade(request, socket, head) {      //handling upgrade(http to websocekt) event
+    console.log("New upgrade")
+    // accepts half requests and rejects half. Reload browser page in case of rejection
+  
+    //if(Math.random() > 0.5){
+    //  return socket.end("HTTP/1.1 401 Unauthorized\r\n", "ascii")     //proper connection close in case of rejection
+    //}
+  
+    //emit connection when request accepted
+    wsServer.handleUpgrade(request, socket, head, function done(ws) {
+      wsServer.emit('connection', ws, request);
+    });
+  });
+
+  const insert_measurement_query = "INSERT INTO measurement(nr,time_stamp,speed,setpoint,pressure,auto,error,co2,rh,temperature) VALUES (?,?,?,?,?,?,?,?,?,?)"
+
+  mqtt_client.on("message", (topic, message) => {
+    // message is Buffer
+    let date = new Date()
+    //console.log(date.toString())
+    //console.log(date.getTime())
+    let time_stamp = date.getTime()
+    let data = JSON.parse(message.toString())
+    data.time_stamp = time_stamp
+  
+    wsServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {     // check if client is ready
+        //client.send(message.toString());
+        client.send(JSON.stringify(data));
+      }
+    })
+  
+    let args = [
+      data.nr,
+      time_stamp,
+      data.speed,
+      data.setpoint,
+      data.pressure,
+      data.auto,
+      data.error,
+      data.co2,
+      data.rh,
+      data.temp
+    ]
+    execute(insert_measurement_query, args)
+    .catch((error) => {
+      console.log(error)
+      console.log("Error, could not insert new value to database")
+    })
+  });
+
+  app.get("/statistics", (req, res) => {
+    res.sendFile(path.resolve("../Client/data.html"))
+  })
+
+  const find_measurements_with_time = "SELECT ? FROM measurement WHERE time_stamp >= ? and time_stamp <= ?"
+  const find_all_measurements = "SELECT ? FROM measurement"
+  
+  async function get_data(query, parameters, start=0, end=0){
+    let data = {}
+  
+    const placeholders = parameters.map(() => '?').join(',');
+  
+    let q;
+    if(start === 0 && end === 0){
+      q = "SELECT * FROM measurement"
+    }
+    else {
+      q = "SELECT * FROM measurement WHERE time_stamp >= ? and time_stamp <= ?"
+    }
+  
+    let new_data = await find_all(q, [start, end])
+  
+    for(let param of parameters){
+      data[param] = []
+      for(let k of new_data){
+        data[param].push(k[param])
+      }
+    }
+  
+    console.log(data)
+    return data
+  }
+  
+  app.get("/statistcs/data", async (req, res) => {
+    console.log(req.query)
+    let args = []
+  
+  
+    let parameters = []
+  
+    parameters.push("time_stamp")
+  
+  
+    if(req.query.temperature){
+      parameters.push("temperature")
+    }
+    if(req.query.pressure){
+      parameters.push("pressure")
+    }
+    if(req.query.rh){
+      parameters.push("rh")
+    }
+    if(req.query.co2){
+      parameters.push("co2")
+    }
+    if(req.query.speed){
+      parameters.push("speed")
+    }
+    if(req.query.setpoint){
+      parameters.push("setpoint")
+    }
+  
+  
+    //console.log(p)
+    //console.log(parameters)
+  
+    let query
+    if(req.query.start === undefined || req.query.end === undefined){
+      query = find_all_measurements
+    }
+    else {
+      query = find_measurements_with_time
+      args.push(req.query.start)
+      args.push(req.query.end)
+    }
+  
+    try{
+      let data = await get_data(query, parameters, parseInt(req.query.start), parseInt(req.query.end))
+      res.send(data)
+    }
+    catch(exception){
+      console.log(exception)
+      res.sendStatus(500)
+    }
+  
+  
+  
+  })
+  
+init_db()
